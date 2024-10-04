2f58c1da85d43781d09fd44b67f3571a7642cb2a
Emiliano Aquetzalli Obregon Reyes <eobregon05@alumnos.uaq.mx>
2024-10-04 13:40:37 -0600

    upload project

---
+++ src/app/new/page.tsx
@@ -0,0 +1,11 @@
+import { TaskForm } from "./TaskForm"
+
+function page() {
+  return (
+    <div className=" flex justify-center items-center h-screen">
+        <TaskForm/>
+    </div>
+  )
+}
+
+export default page
